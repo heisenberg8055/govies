5732cd6475d6424ce1151cf76f9d18fbc828d2e8
Yeswanth <yeswanthsai.inturi@gmail.com>
2025-08-28 12:59:55 +0530

    custom ip based ratelimiter

--- cmd/govies/middleware.go
+++ cmd/govies/middleware.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"sync"
+	"time"
+
+	"github.com/heisenberg8055/govies/internal/realip"
+	"golang.org/x/time/rate"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -15,4 +20,53 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		}()
 		next.ServeHTTP(w, r)
 	})
+}
+
+func (app *application) rateLimit(next http.Handler) http.Handler {
+	switch {
+	case app.config.limiter.enabled:
+
+		type client struct {
+			limiter  *rate.Limiter
+			lastSeen time.Time
+		}
+		var counter = struct {
+			sync.Mutex
+			m map[string]*client
+		}{m: map[string]*client{}}
+
+		// cleaning up inactive ip's every 3 mins
+		go func() {
+			for {
+				time.Sleep(time.Minute)
+				counter.Lock()
+				for ip, client := range counter.m {
+					if time.Since(client.lastSeen) > 3*time.Minute {
+						delete(counter.m, ip)
+					}
+				}
+				counter.Unlock()
+			}
+		}()
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ip := realip.FromRequest(r)
+			counter.Lock()
+			if _, found := counter.m[ip]; !found {
+				counter.m[ip] = &client{limiter: rate.NewLimiter(2, 4)}
+			}
+			counter.m[ip].lastSeen = time.Now()
+			if !counter.m[ip].limiter.Allow() {
+				counter.Unlock()
+				app.rateLimitExceededResponse(w, r)
+				return
+			}
+			counter.Unlock()
+			next.ServeHTTP(w, r)
+		})
+	default:
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			next.ServeHTTP(w, r)
+		})
+	}
 }
